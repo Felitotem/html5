93af28bc07ecc970272d6972e0390a362f63a5da
Felitotem <ismgongon@gmail.com>
2017-03-22 19:49:41 +0000

    Update index.js

--- 3d/js/index.js
+++ 3d/js/index.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   impress().init();
 });
-
-document.addEventListener('impress:stepenter', function(e) {
-  var currentStep = document.querySelector('.active[id]').id;
-  if (currentStep == 'gofirst')
-    impress().goto(0);
-  console.log(currentStep)
-})
